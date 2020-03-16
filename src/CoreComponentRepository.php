a590bcd856c1f0e07fdb9cf3dace5db3dcf46209
Mehedi <mehedi.iitdu@gmail.com>
2020-03-16 14:40:01 +0600

    update

--- src/CoreComponentRepository.php
+++ src/CoreComponentRepository.php
@@ -9,23 +9,23 @@ class CoreComponentRepository
         $rn = self::serializeObjectResponse($gate);
         self::finalizeRepository($rn);
     }
-}
 
-protected static function serializeObjectResponse($zn) {
-    $stream = curl_init();
-    curl_setopt($stream, CURLOPT_SSL_VERIFYPEER, FALSE);
-    curl_setopt($stream, CURLOPT_SSL_VERIFYHOST, 2);
-    curl_setopt($stream, CURLOPT_URL, $zn);
-    curl_setopt($stream, CURLOPT_HEADER, 0);
-    curl_setopt($stream, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($stream, CURLOPT_POST, 1);
-    $rn = curl_exec($stream);
-    curl_close($stream);
-    return $rn;
-}
+    protected static function serializeObjectResponse($zn) {
+        $stream = curl_init();
+        curl_setopt($stream, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($stream, CURLOPT_SSL_VERIFYHOST, 2);
+        curl_setopt($stream, CURLOPT_URL, $zn);
+        curl_setopt($stream, CURLOPT_HEADER, 0);
+        curl_setopt($stream, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($stream, CURLOPT_POST, 1);
+        $rn = curl_exec($stream);
+        curl_close($stream);
+        return $rn;
+    }
 
-protected static function finalizeRepository($rn) {
-    if($rn != 'nice') {
-        return redirect()->to('https://activeitzone.com/check/');
+    protected static function finalizeRepository($rn) {
+        if($rn != 'nice') {
+            return redirect()->to('https://activeitzone.com/check/');
+        }
     }
 }
