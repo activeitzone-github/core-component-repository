faece55b1c3f20e8060fe41e1ed74c72061f6aa5
Mehedi <bsse0607@iit.du.ac.bd>
2021-09-22 20:39:57 +0600

    Update CoreComponentRepository.php

--- src/CoreComponentRepository.php
+++ src/CoreComponentRepository.php
@@ -2,6 +2,7 @@
 
 namespace MehediIitdu\CoreComponentRepository;
 use App\Addon;
+use Cache;
 
 class CoreComponentRepository
 {
@@ -35,25 +36,30 @@ class CoreComponentRepository
                 self::finalizeCache($addon);
             }
     
-            try {
-                $gate = "https://activeitzone.com/activation/check/".$addon->unique_identifier."/".$addon->purchase_code;
-    
-                $stream = curl_init();
-                curl_setopt($stream, CURLOPT_URL, $gate);
-                curl_setopt($stream, CURLOPT_HEADER, 0);
-                curl_setopt($stream, CURLOPT_RETURNTRANSFER, 1);
-                curl_setopt($stream, CURLOPT_POST, 1);
-                $rn = curl_exec($stream);
-                curl_close($stream);
-    
-                if($rn == 'no') {
-                    self::finalizeCache($addon);
+            if(Cache::get($addon->unique_identifier.'-purchased', 'no') == 'no'){
+                try {
+                    $gate = "https://activeitzone.com/activation/check/".$addon->unique_identifier."/".$addon->purchase_code;
+        
+                    $stream = curl_init();
+                    curl_setopt($stream, CURLOPT_URL, $gate);
+                    curl_setopt($stream, CURLOPT_HEADER, 0);
+                    curl_setopt($stream, CURLOPT_RETURNTRANSFER, 1);
+                    curl_setopt($stream, CURLOPT_POST, 1);
+                    $rn = curl_exec($stream);
+                    curl_close($stream);
+        
+                    if($rn == 'no') {
+                        self::finalizeCache($addon);
+                    }
+                    else{
+                        Cache::rememberForever($addon->unique_identifier.'-purchased', function () {
+                            return 'yes';
+                        });
+                    }
+                } catch (\Exception $e) {
+        
                 }
-            } catch (\Exception $e) {
-    
             }
-    
-            return true;
         }
     }
 
